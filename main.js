978643ccc066c4220674f7b63926347cd5331c3c
CodingMonkey75 <harivenkatesh.sridhar@gmail.com>
2025-05-11 23:13:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("App loaded");
 });
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
